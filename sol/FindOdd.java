d65463c3d9a2b63b0310b0243baac44c6350af45
Mvfm <mvfm.filho@hotmail.com>
2025-10-29 16:51:53 -0300

    Retirados imports desnecessários de FindOdd.

--- sol/FindOdd.java
+++ sol/FindOdd.java
@@ -1,9 +1,6 @@
 // Solucionado Dia : 29/10/2025 || Alterado Última vez : 29/10/2025
 // Segue o link para o problema : https://www.codewars.com/kata/54da5a58ea159efa38000836/train/java
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class FindOdd {
     public static int oddInt(int[] ints){
         int found = 0;
