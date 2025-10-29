eb303709cc1a12547c56c4cdc82a5325f35bc7e2
Mvfm <mvfm.filho@hotmail.com>
2025-10-29 15:13:52 -0300

    Melhora de implementação, exemplo incluído.

--- Troll.java
+++ Troll.java
@@ -1,17 +1,13 @@
 // Solucionado dia 29/10/2025
 // Segue o link para o problema : https://www.codewars.com/kata/52fba66badcd10859f00097e/java
 
-import java.util.*;
 public class Troll {
     public static String disemvowel(String str) {
-       ArrayList<String> vowels = new ArrayList<>();
-       vowels.add("a"); vowels.add("A"); vowels.add("e"); vowels.add("E"); vowels.add("i"); vowels.add("I"); vowels.add("o"); vowels.add("O"); vowels.add("u"); vowels.add("U");
-      
-       for (char c : str.toCharArray()){
-         if (vowels.contains(String.valueOf(c))){
-           str = str.replace(String.valueOf(c), "");
-         }
-       }
-       return str;
+        return str.replaceAll("[aAeEiIoOuU]", "");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("String original : 'And you call yourself a communist? LOL!'");
+        System.out.println(disemvowel("And you call yourself a communist? LOL!"));
     }
 }
